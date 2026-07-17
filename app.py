f9e9c9236c4a515dc65b34b4c8db373d6edd1394
Tarika <22f3002758@ds.study.iitm.ac.in>
2026-07-17 12:01:44 +0530

    Milestone: Database Models and Schema Setup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from backend.models import db
+from backend.models import *
+from datetime import datetime
 
 def create_app():
     app=Flask(__name__)
@@ -9,7 +10,35 @@ def create_app():
     return app
 
 app=create_app()
+# db.create_all()
 from backend.routes import *
+from backend.create_initial_data import *
+
+# if Admin.query.first() is None:
+#     admin=Admin(email='admin@gmail.com', password="pass")
+#     db.session.add(admin)
+
+# if Professional.query.first() is None:
+#     prof1=Professional(email='prof1@gmail.com', password='asdf',name='john', address='wert',mobile='98986763',experience='3',status='registered')
+#     prof2=Professional(email='prof2@gmail.com', password='asdf',name='jenny', address='wert',mobile='9898676453',experience='4',status='registered')   
+#     db.session.add_all([prof1,prof2])
+
+# if Package.query.first() is None:
+#     pack1=Package(title='Home Cleaning', price='500', description="afdyewhgfekfh", prof_id=1,status='approved',start_date=datetime(2026,8,2),end_date=datetime(2026,9,2))    
+#     pack2=Package(title='Gardening', price='500', description="afdyewhgfekfh", prof_id=1,status='approved',start_date=datetime(2026,8,2),end_date=datetime(2026,9,2))    
+#     pack3=Package(title='Kitchen Cleaning', price='500', description="afdyewhgfekfh", prof_id=2,status='approved',start_date=datetime(2026,8,2),end_date=datetime(2026,9,2))    
+#     db.session.add_all([pack1,pack2,pack3])
+# db.session.commit()    
+
+# prof_obj=db.session.query(Professional).filter_by(id=1).first()
+# print(prof_obj)
+# print(prof_obj.name)
+
+
+# pack_obj=db.session.query(Package).filter_by(id=1).first()
+# print(pack_obj)
+# print(pack_obj.professional)
+# print(pack_obj.professional.name)
 
 if __name__=="__main__":
     app.run(debug=True)
--- backend/models.py
+++ backend/models.py
@@ -1,2 +1,61 @@
 from flask_sqlalchemy import SQLAlchemy
-db=SQLAlchemy()
+
+db = SQLAlchemy()
+
+
+class Admin(db.Model):
+    
+    id = db.Column(db.Integer, primary_key =True , autoincrement=True)
+    email = db.Column(db.String, unique=True , nullable=False)
+    password = db.Column(db.String , nullable=False)
+    
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key =True , autoincrement=True)
+    email = db.Column(db.String, unique=True , nullable=False)
+    password = db.Column(db.String , nullable=False)
+    name = db.Column(db.String , nullable=False)
+    address = db.Column(db.String, nullable=False)
+    mobile = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False)
+    bookings = db.relationship("Booking", backref="customer",cascade="all, delete-orphan", lazy=True)
+    
+
+
+class Professional(db.Model): # table name professional
+    id = db.Column(db.Integer, primary_key =True , autoincrement=True)
+    email = db.Column(db.String, unique=True , nullable=False)
+    password = db.Column(db.String , nullable=False)
+    name = db.Column(db.String , nullable=False)
+    address = db.Column(db.String, nullable=False)
+    mobile = db.Column(db.String, nullable=False)
+    experience = db.Column(db.String,nullable = False)
+    status = db.Column(db.String, nullable=False)
+    resume_url = db.Column(db.String)
+    packages = db.relationship("Package", backref="professional",cascade="all, delete-orphan", lazy=True) # backref is used to access parent table information from child table
+    bookings = db.relationship("Booking", backref="professional",cascade="all, delete-orphan", lazy=True)
+    
+    
+class Package(db.Model):# table name package
+    id = db.Column(db.Integer, primary_key =True , autoincrement=True)
+    title = db.Column(db.String, unique=True , nullable=False)
+    price = db.Column(db.String , nullable=False)
+    description= db.Column(db.String , nullable=False)
+    prof_id = db.Column(db.Integer ,db.ForeignKey("professional.id") , nullable=False ) # db.ForeignKey("tablename.attribute")
+    status = db.Column(db.String, nullable=False)
+    start_date = db.Column(db.Date , nullable =False)
+    end_date = db.Column(db.Date , nullable =False)
+    bookings = db.relationship("Booking", backref="package",cascade="all, delete-orphan", lazy=True)
+
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key =True , autoincrement=True)
+    
+    total_price = db.Column(db.String , nullable=False)
+    start_time = db.Column(db.Time , nullable =False)
+    end_time = db.Column(db.Time) 
+    date = db.Column(db.Date , nullable =False)
+    status = db.Column(db.String, nullable=False)
+    prof_id = db.Column(db.Integer ,db.ForeignKey("professional.id") , nullable=False )
+    pack_id =  db.Column(db.Integer ,db.ForeignKey("package.id") , nullable=False )
+    cust_id = db.Column(db.Integer ,db.ForeignKey("customer.id") , nullable=False)
